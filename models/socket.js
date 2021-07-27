7843857d88ca1626ab2d7ac7bce9722198d59225
GerardoVR7 <GvrZero_07@outlook.com>
2021-07-27 06:43:32 -0500

    ya quedo la funcionalidad de retornar las cartas para el tablero

--- models/socket.js
+++ models/socket.js
@@ -1,6 +1,7 @@
 const shuffle = require('lodash.shuffle');
 //import shuffle from 'lodash.shuffle';
 const FontAwesomeClasses = require('./fontAwesomeClasses')
+//import FontAwesomeClasses from './fontAwesomeClasses'
 const NUMERO_DE_CARTAS = 20;
 class Sockets {
 
@@ -11,7 +12,13 @@ class Sockets {
     }
 
     mezclarCartas () {
-        const fontAwesomeClasses =  new FontAwesomeClasses();
+        const fontAwesomeClasses =  ["fa-adjust", "fa-adn", "fa-align-center", "fa-align-justify", "fa-align-left", "fa-align-right",
+        "fa-ambulance", "fa-anchor", "fa-android", "fa-angellist", "fa-angle-double-down", "fa-angle-double-left",
+        "fa-angle-double-right", "fa-angle-double-up", "fa-angle-down", "fa-angle-left", "fa-angle-right", "fa-angle-up",
+        "fa-apple", "fa-archive", "fa-area-chart", "fa-arrow-circle-down", "fa-arrow-circle-left", "fa-arrow-circle-o-down",
+        "fa-arrow-circle-o-left", "fa-arrow-circle-o-right", "fa-arrow-circle-o-up", "fa-arrow-circle-right", "fa-arrow-circle-up",
+        "fa-arrow-down", "fa-arrow-left", "fa-arrow-right", "fa-arrow-up", "fa-arrows", "fa-arrows-alt", "fa-arrows-h",
+        "fa-arrows-v", "fa-asterisk", "fa-at", "fa-automobile", "fa-backward", "fa-ban"]
         
         let cartas = [];
     
